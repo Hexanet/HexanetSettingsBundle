244e02ef2f479387e718e9434d1bb0ce8c75963f
Nicolas Hart <n.hart@hexanet.fr>
2018-03-22 16:09:03 +0100

    Use !taggued instead of compiler pass to register schema

--- src/HexanetSettingsBundle.php
+++ src/HexanetSettingsBundle.php
@@ -2,16 +2,8 @@
 
 namespace Hexanet\SettingsBundle;
 
-use Hexanet\SettingsBundle\DependencyInjection\Compiler\RegisterSchemasPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class HexanetSettingsBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new RegisterSchemasPass());
-    }
 }
